d2fb257b96c7fc23b7f8b05dec01e8e2ed9a7d67
Swonk12 <polruizmarin@gmail.com>
2024-09-20 19:37:25 +0200

    Version Avanzada Slidder Menu 1

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ function mostrarFecha() {
     let mesActual = fechaActual.getMonth() + 1; // Los meses empiezan en 0
     let añoActual = fechaActual.getFullYear();
 
-    // Obtener el día de la semana
-    let diasSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
-    let diaSemana = diasSemana[fechaActual.getDay()]; // Día de la semana en texto
-
     // Mostrar la fecha en el elemento con id="fecha"
-    document.getElementById('fecha').innerText = `Hoy es ${diaSemana}, ${diaActual}/${mesActual}/${añoActual}`;
+    document.getElementById('fecha').innerText = `Fecha: ${diaActual}/${mesActual}/${añoActual}`;
 }
+
+// Ejecutar la función cuando la página haya cargado completamente
+window.onload = mostrarFecha;
