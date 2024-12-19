92227e331a4aa6d80eabe3c0a0612dcc12c66fd2
BRVallmi <blai.vallmitjana@gmail.com>
2024-12-19 15:41:10 +0100

    vainas varias

---
+++ js/slick.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider').slick({
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots: true,
+      infinite: true,
+      arrows: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+  });
